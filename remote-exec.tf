dd4ef8855c81493a546023903141ebaba045297f
Linu <blinu.1997@gmail.com>
2022-12-08 02:09:25 +0530

    Installed apache via remote-exec

--- remote-exec.tf
+++ remote-exec.tf
@@ -1,14 +1,49 @@
 
 provider "aws" {
 	region = "us-east-1"
-	access_key = "AKIA57L3YLDEJFUZPHNU"
-	secret_key = "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"
+	access_key = "demo"
+	secret_key = "demo"
 }
+
+resource "aws_security_group" "remote_exec_sg" {
+	name = "SG-1"
+	
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	ingress {
+		from_port = 443
+		to_port = 443
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+}
+
 
 resource "aws_instance" "myec2" {
 	ami = "ami-0574da719dca65348"
 	instance_type = "t2.micro"
 	key_name = "ubuntu-terraform"
+	security_groups = [aws_security_group.remote_exec_sg.name]
 
 	connection {
     type     = "ssh"
